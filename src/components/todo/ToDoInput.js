b1e0420dff2f71998f83f90ad25d0ff9b4656722
Ülfet <ukacay87@gmail.com>
2022-09-09 00:34:36 +0300

    added todo input

--- src/components/todo/ToDoInput.js
+++ src/components/todo/ToDoInput.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useId, useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 const ToDoInput = () => {
+  const dispatch = useDispatch();
+  const [toDoItem,setToDoItem] = useState({
+    id:useId(),
+    content:"",
+    isCompleted:false,
+  });
+  const handleNewToDo = (e)=>{
+    const {name,value} = e.target; 
+    setToDoItem({...toDoItem,[name]:value});
+  }
+  const AddToDo = ()=>{
+    console.log(toDoItem)
+  }
   return (
     <div className='todo-input-wrapper'>
-      <input type="text" name="todo-input" id="todo-input" className='to-do' placeholder='Enter a todo...' />
-      <button id='btn-add-todo'><i className="fa-solid fa-plus"></i></button>
+      <input onChange={handleNewToDo} value={toDoItem.content} type="text" name="toDoİtem" id="toDoİtem" className='to-do' placeholder='Enter a todo...' />
+      <button id='btn-add-todo' onClick={AddToDo}><i className="fa-solid fa-plus"></i></button>
     </div>
   )
 }
